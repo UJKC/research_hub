4083d9bff9cf22d1515b5cbd5092978a8b6441a5
Ujwal KC <ujwalkcsps@gmail.com>
2024-01-15 09:48:09 +0530

    login success

--- researchhome/src/components/Login.jsx
+++ researchhome/src/components/Login.jsx
@@ -1,57 +1,52 @@
 import React from 'react';
 import '../styles/Sign.css'
 
-function Sign() {
+function Login() {
     return (
-      <section class="vh-100 bg-image"
-        style={{backgroundImage: "url('https://mdbcdn.b-cdn.net/img/Photos/new-templates/search-box/img4.webp')"}}>
-        <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-          <div class="container h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-              <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                <div class="card" style={{borderRadius: 15}}>
-                  <div class="card-body p-5">
-                    <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+      <section class="vh-100">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+              <div class="card" style={{borderRadius: '1rm'}}>
+                <div class="row g-0">
+                  <div class="col-md-6 col-lg-5 d-none d-md-block">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                      alt="login form" class="img-fluid" style={{borderRadius: '1rem 0 0 1rem'}} />
+                  </div>
+                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
 
-                    <form>
+                      <form>
 
-                      <div class="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example1cg">Your Name</label>
-                      </div>
+                        <div class="d-flex align-items-center mb-3 pb-1">
+                          <i class="fas fa-cubes fa-2x me-3" style={{color: '#ff6219'}}></i>
+                          <span class="h1 fw-bold mb-0">Logo</span>
+                        </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example3cg">Your Email</label>
-                      </div>
+                        <h5 class="fw-normal mb-3 pb-3" style={{letterSpacing: '1px'}}>Sign into your account</h5>
 
-                      <div class="form-outline mb-4">
-                        <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example4cg">Password</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                          <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example17">Email address</label>
+                        </div>
 
-                      <div class="form-outline mb-4">
-                        <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-                        <label class="form-label" for="form3Example4cdg">Repeat your password</label>
-                      </div>
+                        <div class="form-outline mb-4">
+                          <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                          <label class="form-label" for="form2Example27">Password</label>
+                        </div>
 
-                      <div class="form-check d-flex justify-content-center mb-5">
-                        <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3cg" />
-                        <label class="form-check-label" for="form2Example3g">
-                          I agree all statements in <a href="#!" class="text-body"><u>Terms of service</u></a>
-                        </label>
-                      </div>
+                        <div class="pt-1 mb-4">
+                          <button class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                        </div>
 
-                      <div class="d-flex justify-content-center">
-                        <button type="button"
-                          class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
-                      </div>
+                        <a class="small text-muted" href="#!">Forgot password?</a>
+                        <p class="mb-5 pb-lg-2" style={{color: '#393f81'}}>Don't have an account? <a href="#!"
+                            style={{color: '#393f81'}}>Register here</a></p>
+                        <a href="#!" class="small text-muted">Terms of use.</a>
+                        <a href="#!" class="small text-muted">Privacy policy</a>
+                      </form>
 
-                      <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
-                          class="fw-bold text-body"><u>Login here</u></a></p>
-
-                    </form>
-
+                    </div>
                   </div>
                 </div>
               </div>
@@ -62,4 +57,4 @@ function Sign() {
     );
   }
   
-  export default Sign;
+  export default Login;
