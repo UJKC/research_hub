b634f1d5c62fc31f7e82a28fe53e00f54e9b891c
Ujwal KC <ujwalkcsps@gmail.com>
2024-01-25 00:11:34 +0530

    very responsive

--- social/src/components/Likedcomponent.jsx
+++ social/src/components/Likedcomponent.jsx
@@ -1,85 +1,44 @@
 import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const Liked = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
 
   return (
     <div>
-    <div class="row d-none d-md-flex">
-        <div class="col-md-2">
-          <button class="btn btn-primary btn-sm">Liked <span class="badge badge-light">2/12</span></button>
-        </div>
-        <div class="col-md-2">
-          <button class="btn btn-info btn-sm">Comment <span class="badge badge-light">2/12</span></button>
-        </div>
-        <div class="col-md-6">
-          <span class="badge badge-secondary">
-          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Tagged
-</button>
+      <Button variant="primary" className="col-md-2 col-sm-3">
+        Like
+      </Button>
+      <Button variant="secondary" className="col-md-2 col-sm-3">
+        Comment
+      </Button>
+      <Button variant="info" className="col-md-6 col-sm-3" onClick={handleShowModal}>
+        Tag
+      </Button>
+      <Button variant="success" className="col-md-2 col-sm-3">
+        Bookmark
+      </Button>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-          </span>
-        </div>
-        <div class="col-md-2">
-          <button class="btn btn-warning btn-sm">Bookmark</button>
-        </div>
-      </div>
-
-      <div class="row d-md-none">
-        <div class="col-3">
-          <button class="btn btn-primary btn-sm">Liked <span class="badge badge-light">3/12</span></button>
-        </div>
-        <div class="col-3">
-          <button class="btn btn-info btn-sm">Comment <span class="badge badge-light">3/12</span></button>
-        </div>
-        <div class="col-3">
-          <span class="badge badge-secondary"><div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Tagged
-</button>
-
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div></span>
-        </div>
-        <div class="col-3">
-          <button class="btn btn-warning btn-sm">Bookmark</button>
-        </div>
-      </div>
-
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tag Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {/* Add content for your modal here */}
+          This is the content of the Tag modal.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseModal}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
